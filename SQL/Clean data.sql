15891383b1ef55309c7d99e0e3644d1d411ec9b1
echaore35-bit <echaore35@gmail.com>
2025-11-07 11:33:39 +0800

    Add files via upload

---
+++ SQL/Clean data.sql
@@ -0,0 +1,79 @@
+-- See if table exists and check schema 
+
+SELECT * 
+FROM INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_NAME = 'layoff_data_staging';
+
+-- Count rows if the count is the same on file
+SELECT COUNT(*) 
+FROM layoff_data_staging;
+
+-- See first 10 rows of the data to see the column headers and first fewm data
+SELECT TOP 10 *
+FROM layoff_data_staging;
+
+-- Check Duplicates and build cte 
+
+CREATE OR ALTER VIEW vw_layoff_data_cleaned AS
+WITH duplicate_cte AS
+(
+    SELECT *, 
+        ROW_NUMBER() OVER(
+            PARTITION BY company, [location], industry, total_laid_off, [date], stage, country, funds_raised_millions
+            ORDER BY company
+        ) AS rn
+    FROM layoff_data_staging
+),
+
+-- TRIM the text columns but keep all other columns
+trim_cte AS
+(
+    SELECT
+        TRIM(company) AS company,
+        TRIM([location]) AS [location],
+        TRIM(industry) AS industry,
+        total_laid_off,
+        percentage_laid_off,
+        [date],
+        TRIM(stage) AS stage,
+        TRIM(country) AS country,
+        funds_raised_millions,
+        rn
+    FROM duplicate_cte
+),
+
+-- Check all minor changes in all columns, nulls,convert data types and standardized it
+standardized_industry_cte AS
+(
+    SELECT
+        company,
+        [location],
+        CASE 
+            WHEN industry LIKE 'Crypto%' THEN 'Crypto'
+            WHEN industry LIKE 'Fin-Tech%' THEN 'Finance'
+            WHEN industry LIKE 'HR%' THEN 'Human Resources'
+            ELSE industry
+        END AS industry,
+        TRY_CAST(NULLIF(total_laid_off, 'NULL') AS INT) AS total_laid_off,
+        TRY_CAST(NULLIF(percentage_laid_off, 'NULL') AS FLOAT) AS percentage_laid_off,
+        TRY_CAST([date] AS DATE) AS [date],
+        stage,
+        REPLACE(country,'.','') AS country,
+        TRY_CAST(NULLIF(funds_raised_millions, 'NULL') AS INT) AS funds_raised_millions, 
+        rn
+    FROM trim_cte
+),
+-- Final Ouput | use rn = 1 so that duplicates are filtered
+final_data AS(
+    SELECT *
+    FROM standardized_industry_cte
+    WHERE rn = 1
+)
+
+SELECT * 
+FROM final_data
+
+-- Check if the data has correct data type
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'vw_layoff_data_cleaned';
---
+++ SQL/Report Query.sql
@@ -0,0 +1,51 @@
+/*-----------------------------------------
+	Data Reporting
+*/-----------------------------------------
+
+-- 1.Total layoffs per company
+SELECT TOP 10 company, sum(total_laid_off) AS total_laidoff_company
+FROM vw_layoff_data_cleaned
+GROUP BY company 
+ORDER BY total_laidoff_company DESC
+
+-- 2.Total layoffs per industry
+SELECT TOP 10 industry, sum(total_laid_off) AS total_laidoff_industry
+FROM vw_layoff_data_cleaned
+GROUP BY industry
+ORDER BY total_laidoff_industry DESC
+
+-- 3.Top 5 countries with the highest layoffs
+SELECT TOP 5 country, sum(total_laid_off) AS total_laidoff_country
+FROM vw_layoff_data_cleaned
+GROUP BY country
+ORDER BY total_laidoff_country DESC
+
+-- 4.Year with the most layoffs
+SELECT YEAR(date) AS year_layoff, sum(total_laid_off) AS total_laidoff_year
+FROM vw_layoff_data_cleaned
+WHERE [date] IS NOT NULL
+GROUP BY YEAR(date)
+ORDER BY total_laidoff_year DESC
+
+
+-- 5.Company with the highest single layoff event
+SELECT TOP 1 company, total_laid_off, [date], country
+FROM vw_layoff_data_cleaned
+WHERE total_laid_off IS NOT NULL
+ORDER BY total_laid_off DESC;
+
+-- 6.Trend of layoffs by month 
+SELECT 
+    FORMAT([date], 'yyyy-MM') AS month,
+    SUM(total_laid_off) AS total_layoffs
+FROM vw_layoff_data_cleaned
+WHERE [date] IS NOT NULL
+GROUP BY FORMAT([date], 'yyyy-MM')
+ORDER BY month,total_layoffs DESC;
+
+-- View of all data
+SELECT * FROM vw_layoff_data_cleaned
+
+
+
+
